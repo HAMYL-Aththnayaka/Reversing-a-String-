da5aa4a9acf43abd3b2d2ef57d8d474b95eb4232
Yasas Aththanayaka <Aththanayakayasas@gmail.com>
2024-11-10 20:19:52 +0530

    Using C# Language Taking  a String and putting it out  in Reveres

---
+++ Reverse String/Reverse String/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        /* ask a user to input Mesage
+         * print in order
+         * print in reverse*/
+
+        {
+            Console.Write("Enter your Message: ");
+            string message = Console.ReadLine();
+
+
+          
+            Console.WriteLine("The Message Length is : "+message.Length);
+            Console.WriteLine();
+
+            for (int i = 0; i < message.Length;  i++)
+            {
+                Console.Write(message[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Reverse :");
+            Console.WriteLine();
+
+
+            for (int i = message.Length-1; i > -1 ; i--)
+            {
+                Console.Write(message[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
